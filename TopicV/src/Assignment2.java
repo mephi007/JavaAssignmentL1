6643b61d3cedbe9d9e8f64c34d98a2348f294dfc
Sumit Kumar Roy (Communications-Telecom Equipment) <SU20051907@wipro.com>
2019-01-16 16:59:03 +0530

    Assignment5

---
+++ TopicV/src/Assignment2.java
@@ -0,0 +1,16 @@
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length == 2)
+		{
+		System.out.println(args[0]+" Technologies "+ args[1]);
+		}
+		else
+		{
+			System.out.println(args[0]+" Technologies");
+		}
+	}
+
+}
